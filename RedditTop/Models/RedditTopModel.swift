e42f7ac20213c600115c3e2260ecaa52925a346b
Andrii Syrota <andyorphandev@gmail.com>
2021-02-17 15:44:18 +0200

    Added main models

--- RedditTop/Models/RedditTopModel.swift
+++ RedditTop/Models/RedditTopModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+
+struct RedditTop: Codable {
+    let kind: String?
+    let data: RedditTopData?
+}
+
+struct RedditTopData: Codable {
+    let children: [Child]?
+    let after: String?
+    let before: String?
+}
+
+struct Child: Codable {
+    let kind: String?
+    let data: ChildData?
+}
+
+struct ChildData: Codable {
+    let title: String?
+    let authorFullname: String?
+    let name: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case authorFullname = "author_fullname"
+        case title
+        case name
+    }
+}
