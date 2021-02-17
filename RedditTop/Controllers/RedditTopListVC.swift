019b766079243c6f69f5f0366e93f68ca51f0808
Andrii Syrota <andyorphandev@gmail.com>
2021-02-17 16:25:39 +0200

    basic ui + test request

---
+++ RedditTop/Controllers/RedditTopListVC.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  RedditTop
+//
+//  Created by Andy Orphan on 17.02.2021.
+//
+
+import UIKit
+
+class RedditTopListVC: UIViewController {
+    
+    @IBOutlet weak var topListTableView: UITableView!
+    
+    private var redditTopChilds = [Child]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        RedditTopNetworkManager.getRedditTop { (result, response, error) in
+            if let result = result {
+                self.redditTopChilds = result.data?.children ?? [Child]()
+                self.topListTableView.reloadData()
+            }
+        }
+    }
+}
+
+//MARK: - TableView
+extension RedditTopListVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return redditTopChilds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RedditTopListCell", for: indexPath) as? RedditTopListCell else { return UITableViewCell() }
+        
+        let redditTopChild = redditTopChilds[indexPath.row]
+        
+        cell.configureWith(redditTopChild)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+}
---
+++ RedditTop/TableViewCells/RedditTopListCell.swift
@@ -0,0 +1,28 @@
+//
+//  RedditTopListCell.swift
+//  RedditTop
+//
+//  Created by Andy Orphan on 17.02.2021.
+//
+
+import UIKit
+
+class RedditTopListCell: UITableViewCell {
+
+    @IBOutlet weak var postTitleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureWith(_ model: Child) {
+        self.postTitleLabel.text = model.data?.title
+    }
+
+}
