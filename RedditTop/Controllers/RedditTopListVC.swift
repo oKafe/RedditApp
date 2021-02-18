97dd2a861d5da20314303e1bc810c333b8b1afff
Andrii Syrota <andyorphandev@gmail.com>
2021-02-18 17:13:57 +0200

    pull to refresh

--- RedditTop/Controllers/RedditTopListVC.swift
+++ RedditTop/Controllers/RedditTopListVC.swift
@@ -10,16 +10,21 @@ import UIKit
 class RedditTopListVC: UIViewController {
     
     @IBOutlet weak var topListTableView: UITableView!
+    private var refreshControl = UIRefreshControl()
     
     private var redditTopChilds: [Model<Child>]?
-    
     private let dataRequestsManager = RedditTopListRequestsManager(limit: 20)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataRequestsManager.refresh { (result, error) in
-            self.updateDataList(model: result)
-        }
+        refreshData()
+        configureRefreshControll()
+    }
+    
+    private func configureRefreshControll() {
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(self.refreshControllAction), for: .valueChanged)
+        topListTableView.addSubview(refreshControl)
     }
     
     private func updateDataList(model: Model<RedditTop>?) {
@@ -32,12 +37,26 @@ class RedditTopListVC: UIViewController {
         topListTableView.reloadData()
     }
     
+    private func refreshData() {
+        dataRequestsManager.refresh { (result, error) in
+            self.updateDataList(model: result)
+            self.refreshControl.endRefreshing()
+        }
+    }
+    
     private func loadNext() {
         dataRequestsManager.loadNext { (result, error) in
             self.updateDataList(model: result)
         }
     }
-    
+}
+
+//MARK: - Actions
+extension RedditTopListVC {
+    @objc func refreshControllAction() {
+        self.redditTopChilds = nil
+        refreshData()
+    }
 }
 
 //MARK: - TableView
--- RedditTop/Network/Router.swift
+++ RedditTop/Network/Router.swift
@@ -9,8 +9,6 @@ import Foundation
 
 enum HTTPMethod: String {
     case get = "GET"
-    case post = "POST"
-    ///
 }
 
 protocol Router {
